f580ab68dda8acf0b57de849db485dc7a2cf479d
Joe <josephfmck@gmail.com>
2021-09-09 16:48:22 -0500

    Start contact keeper React project, setup dependencies, create server.js for backend server

---
+++ server.js
@@ -0,0 +1,16 @@
+//*backend entry point 
+//* TERM: npm run server - to run backend server
+//import
+const express = require('express');
+
+//init express
+const app = express();
+
+//endpoint/route
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API...'}));
+
+
+//env var PORT production OR 5000 development
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
