eb781c77505a96fa28a4bd432547e13f4b62851a
Joe <josephfmck@gmail.com>
2021-09-15 14:59:21 -0500

    backendSetup Branch

--- server.js
+++ server.js
@@ -25,3 +25,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
+
+
+
+//!backendSetup branch
