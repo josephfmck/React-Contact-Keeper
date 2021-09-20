848a3995453f6fe13fc7679dd1272a0b44175720
Joe <josephfmck@gmail.com>
2021-09-20 15:01:18 -0500

    END 43, Register a new User and Hash password, In users.js Route: set POST to async for express-validator and bcrypt, validate req.body fields, try catch to register new user, check if user with email already exists, if not then create new User with model, encrypt password and save new User to DB, NOW will save User registration data in DB Collection

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 //endpoint      http://localhost:5000/api/users
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
 
 //* import the user DB model to use in route
@@ -20,7 +21,7 @@ router.post(
   [body("name", "Please add name").not().isEmpty(),
     body("email", "Please include valid email").isEmail(),
     body("password", "Please enter a password with 6 or more characters").isLength({ min: 6 })],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     //if errors not empty 
     if(!errors.isEmpty()) {
@@ -28,9 +29,40 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed validation');
+    
     //? req.body needs express middleware in server.js
-    // res.send(req.body);
+    //destruct fields from req.body
+    const { name , email, password } = req.body;
+
+    try {
+        //check if already user with email
+        //find user by email DB field
+        let user = await User.findOne({ email: email });
+        if(user) {
+            res.status(400).json({ msg: "User already exists" });
+        }
+
+        //*create new user with model User
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        //* encrypt password
+        //generate salt with 10 rounds
+        const salt = await bcrypt.genSalt(10);
+        //set password to hashed password
+        user.password = await bcrypt.hash(password, salt);
+    
+        //* save user to DB
+        await user.save();
+
+        res.send('User Saved');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
   }
 );
 
